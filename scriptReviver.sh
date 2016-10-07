5791d67d72f6764f27ad441bdecd302562adf05b
LPompe <Lucasvhspompe@gmail.com>
2016-10-07 13:50:15 +0000

    minor changes

--- scriptReviver.sh
+++ scriptReviver.sh
@@ -1,4 +1,6 @@
-until python processmanager.py > /data/logs/cameralog 1>&2 &; do
+#!/bin/sh -
+until python /home/pi/git/koelkast/processmanager.py > /data/logs/cameralog; do
     echo "Main program loop crashed $?.  Respawning.." >&2 >> /data/logs/systemlog.log
+    echo "Main program loop crashed $?.  Respawning.." >&2
     sleep 10
 done
