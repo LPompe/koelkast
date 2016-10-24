2c9bbd5cd4af786ea5811695bd0121b3afe1eff7
lucas <lucasvhspompe@gmail.com>
2016-10-24 21:41:27 +0200

    rollbar bugfix

--- rollbar_report.py
+++ rollbar_report.py
@@ -2,5 +2,5 @@ import rollbar
 import sys
 import json
 from extra_utils import get_secrets
-rollbar.init(get_secrets['rollbar_token'], 'production')
+rollbar.init(get_secrets()['credentials']['rollbar_token'], 'production')
 rollbar.report_message('System crash: ' + sys.argv[1], 'critical')
