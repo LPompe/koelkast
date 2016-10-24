eb0262b94665df0e5c6fd67a9ec9d6b5883c8349
lucas <Lucasvhspompe@gmail.com>
2016-10-24 17:32:32 +0200

    added rollbar reporting support

--- scriptReviver.sh
+++ scriptReviver.sh
@@ -2,5 +2,6 @@
 until python /home/pi/git/koelkast/processmanager.py > /data/logs/cameralog; do
     echo "Main program loop crashed $?.  Respawning.." >&2 >> /data/logs/systemlog.log
     echo "Main program loop crashed $?.  Respawning.." >&2
+    python /home/pi/git/koelkast/rollbar_report.py $?
     sleep 10
 done
