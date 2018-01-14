5974d552183aefab02fac12d5e749e71ddeff6d5
LPompe <lucasvhspompe@gmail.com>
2018-01-14 14:58:31 +0100

    small documentation change

--- observer.sh
+++ observer.sh
@@ -1,3 +1,5 @@
+#!/bin/sh
+# add this script to crontab ($crontab -e) to run it on startup
 until python3 main.py >> systemlog.log 2>&1; do
     echo "Main program loop crashed $?.  Respawning.." >&2 >> systemlog.log
     echo "Main program loop crashed $?.  Respawning.." >&2
